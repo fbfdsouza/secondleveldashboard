a3ae6a4088b84b5f3763c337bd817b4c1c880468
franciscobrunofilgueiras <fbfdsouza@gmail.com>
2019-08-12 14:13:30 -0300

    sliding case list

--- src/components/ProfileBoard/ProfileBoard.js
+++ src/components/ProfileBoard/ProfileBoard.js
@@ -2,8 +2,20 @@ import React from "react";
 import "./style/ProfileBoard.css";
 import CaseList from "../CaseList/CaseList";
 import CaseItem from "../CaseItem/CaseItem";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const ProfileBoard = ({ children, name, headerColor, borderColor }) => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true
+  };
+
   return (
     <div className="profile_container" style={{ border: borderColor }}>
       <div
@@ -14,13 +26,26 @@ const ProfileBoard = ({ children, name, headerColor, borderColor }) => {
         <div>{name}</div>
       </div>
       <div className="profile_container_content">
-        <CaseList>
-          <CaseItem caseNumber="16815" caseClient="Solar" color="red" />
-          <CaseItem caseNumber="17087" caseClient="PSP" color="red" />
-          <CaseItem caseNumber="17412" caseClient="Kof-Br" color="orange" />
-          <CaseItem caseNumber="17298" caseClient="G.Simões" color="green" />
-          <CaseItem caseNumber="18077" caseClient="SHV" color="green" />
-        </CaseList>
+        <Slider {...settings}>
+          <CaseList>
+            <CaseItem caseNumber="16815" caseClient="Solar" color="red" />
+            <CaseItem caseNumber="17087" caseClient="PSP" color="red" />
+            <CaseItem caseNumber="17412" caseClient="Kof-Br" color="orange" />
+            <CaseItem caseNumber="17298" caseClient="G.Simões" color="green" />
+            <CaseItem caseNumber="18077" caseClient="SHV" color="green" />
+          </CaseList>
+          <CaseList>
+            <CaseItem caseNumber="16888" caseClient="Dawnfoods" color="red" />
+            <CaseItem caseNumber="15432" caseClient="Reyes" color="red" />
+            <CaseItem caseNumber="14258" caseClient="CVI" color="orange" />
+            <CaseItem caseNumber="13251" caseClient="RNDC" color="green" />
+            <CaseItem
+              caseNumber="1982"
+              caseClient="Pepsico-gtm"
+              color="green"
+            />
+          </CaseList>
+        </Slider>
       </div>
     </div>
   );
